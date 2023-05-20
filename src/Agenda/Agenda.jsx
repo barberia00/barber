0ca1225a161a51e8abf0d25850566e0c371b76e8
sebastiangib <sebastianberrio0627@gmail.com>
2023-05-20 13:16:59 -0500

    se valida formulario y gif carga home

--- src/Agenda/Agenda.jsx
+++ src/Agenda/Agenda.jsx
@@ -1,8 +1,64 @@
 import 'animate.css'
 import './Agenda.css'
 import WOW from 'wow.js'
-import { useEffect } from 'react'
+import { useEffect,useState } from 'react'
+import Swal from 'sweetalert2'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+
 export function Agenda(){
+
+    const[nombre,setNombre]=useState(null)
+    const[correo,setCorreo]=useState(null)
+    const[telefono,setTelefono]=useState(null)
+    const[fecha,setFecha]=useState(null)
+    const[hora,setHora]=useState(null)
+
+    const[errores,setErrores]=useState({})
+
+    useEffect(function(){
+        if(Object.keys(errores).length>0){ // Aca tengo errores
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Tienes campos sin rellenar!',
+              })
+        }else{ // No hay errores
+            Swal.fire(
+                'Listo!',
+                'Se ha reservado tu cita satisfactoriamente!',
+                'success'
+              )
+        }
+    },[errores])
+
+    function validarFormulario(evento){
+        evento.preventDefault()
+        let listaErrores= {}
+
+        if(!nombre){
+         listaErrores.nombre="El nombre es obligatorio"   
+        }
+
+        if(!correo){
+            listaErrores.correo="El correo es obligatorio"
+        }
+
+        if(!telefono){
+            listaErrores.telefono="El telefono es obligatorio"   
+           }
+
+        if(!fecha){
+            listaErrores.fecha="La fecha es obligatoria"   
+           }
+
+        if(!hora){
+            listaErrores.hora="La Hora es obligatoria"   
+           }
+
+        setErrores(listaErrores)
+
+    }
 
 useEffect(function(){
         const wow = new WOW()
@@ -14,7 +70,7 @@ useEffect(function(){
         <main>
             <section>
                 <div className="bannerAgenda">
-                <form>
+                <form onSubmit={validarFormulario}>
                          <div className='formulario'>
                             <h1 className='text-center text-white wow animate_animated animate_fadeInDownBig'>RESERVA TU TURNO</h1>
                             <div className="row">
@@ -23,37 +79,75 @@ useEffect(function(){
                                         <span className="input-group-text" id="basic-addon1">
                                             <i className="bi bi-person-circle"></i>
                                         </span>
-                                        <input type="text" className="form-control" placeholder="Nombre Cliente" />
+                                        <input 
+                                         type="text" 
+                                         className={`form-control ${errores.nombre?'is-invalid':''}`}
+                                         placeholder="Nombre Cliente"
+                                         id='nombre'
+                                         value={nombre}
+                                         onChange={(evento)=>{
+                                             setNombre(evento.target.value)
+                                         }} />
+                                         <div class="invalid-feedback">Por favor digite tu nombre.</div>
                                     </div>
                                 </div>
                                 <div className="col-12 col-md-6">
                                     <div className="input-group mb-3">
                                         <span className="input-group-text" id="basic-addon1"><i class="bi bi-envelope-check-fill"></i></span>
-                                        <input type="text" className="form-control" placeholder="Correo cliente" />
+                                        <input 
+                                         type="email" 
+                                         className={`form-control ${errores.correo?'is-invalid':''}`} 
+                                         value={correo}
+                                         placeholder="Correo cliente"
+                                         onChange={(evento)=>{
+                                    setCorreo(evento.target.value)
+                                }} />
+                                <div class="invalid-feedback">Por favor digite tu correo.</div>
                                     </div>
                                 </div>
                             </div>
-
-<div className="row">
+                                <div className="row">
                                 <div className="col-12">
                                     <div className="input-group mb-3">
                                         <span className="input-group-text" id="basic-addon1"><i class="bi bi-phone-fill"></i></span>
-                                        <input type="text" className="form-control" placeholder="Telefono Cliente" />
+                                        <input 
+                                        type="number" 
+                                        className={`form-control ${errores.telefono?'is-invalid':''}`} 
+                                        value={telefono}
+                                        placeholder="Telefono Cliente"
+                                        onChange={(evento)=>{
+                                            setTelefono(evento.target.value)
+                                        }} />
+                                        <div class="invalid-feedback">Por favor digite tu número.</div>
                                     </div>
                                 </div>
                             </div>
-
-<div className="row">
+                               <div className="row">
                                 <div className="col-12 col-md-6">
                                     <div className="input-group mb-3">
                                         <span className="input-group-text" id="basic-addon1"><i class="bi bi-calendar-check-fill"></i></span>
-                                        <input type="date" className="form-control" placeholder="Telefono Cliente" />
+                                        <input 
+                                         type="date" 
+                                         className={`form-control ${errores.fecha?'is-invalid':''}`} 
+                                         value={fecha}
+                                         onChange={(evento)=>{
+                                            setFecha(evento.target.value)
+                                        }} />
+                                        <div class="invalid-feedback">Por favor selecciona una fecha.</div>
                                     </div>
                                 </div>
+
                                 <div className="col-12 col-md-6">
                                     <div className="input-group mb-3">
-                                        <span className="input-group-text" id="basic-addon1"><i class="bi bi-clock-fill"></i></span>
-                                        <select className="form-select"  defaultValue={'DEFAULT'}>
+                                        <span className="input-group-text" id="basic-addon1"><i class="bi bi-alarm-fill"></i></span>
+                                        <select
+                                            id='hora'
+                                            onChange={(evento) => {
+                                                setHora(evento.target.value);
+                                              }}
+                                            className={`form-select ${errores.hora? "is-invalid" : ""}`} 
+                                            value={hora}
+                                            defaultValue={'DEFAULT'}>
                                             <option value="DEFAULT">Hora:</option>
                                             <option value="1">6:30 am</option>
                                             <option value="2">7:00 am</option>
@@ -65,6 +159,7 @@ useEffect(function(){
                                             <option value="3">5:30 am</option>
                                             <option value="3">10:30 am</option>
                                         </select>
+                                           <div class="invalid-feedback">Por favor selecciona una hora.</div>
                                     </div>
                                 </div>
                                
---
+++ src/shared/Carga/Carga.jsx
@@ -0,0 +1,23 @@
+import './Carga.css'
+export function Carga(){
+
+    return(
+        <>
+
+            <div className="container fondo">
+                <div className="row justify-content-center">
+                    <div className="col-12 col-md-5 text-center text-white">
+
+                        <h2>Bienvenido a MagicBarber</h2>
+                        <img src="../../src/assets/img/gif home.gif" alt="gif" className="mx-auto d-block img-fluid"/>
+                        <p>
+                        ¡El mundo mágico de MagicBarber! Donde tu estilo se transforma en una obra de arte <br /> y te atendemos de la mejor manera!
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        
+        </>
+    )
+}
